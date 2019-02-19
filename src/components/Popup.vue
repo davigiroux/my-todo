fb2190184aa29220cf113aa2cea1ece24065147b
davigiroux <daviGA240296>
2019-02-19 10:33:43 -0400

    Terminando todo

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -51,6 +51,7 @@ export default {
                 db.collection('projects').add(project).then(() => {
                     this.loading = false
                     this.dialog = false
+                    this.$emit('projectAdded')
                 })
             }
         }
